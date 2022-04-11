af6f328a68a09f790cd0e36d85e0c5c41440f4d2
avena <avena@avena>
2022-04-11 14:56:09 +0100

    Remove unused commented lines; add check for JtcStatus when parsing serial frame

--- rs485_com.py
+++ rs485_com.py
@@ -46,16 +46,13 @@ class RSComm:
             return
         if len(self.read_bytes_buffer) > params.COMBUFREADMAX:
             self.read_bytes_buffer = b''
-            # print('len(self.read_bytes_buffer) > params.COMBUFREADMAX')
             return
-        if (header_idx := self.read_bytes_buffer.find(params.Host_FT['Header'].value)) > params.COMBUFREADMAX - 4:
+        if (header_idx := self.read_bytes_buffer.find(params.Host_FT.Header.value)) > params.COMBUFREADMAX - 4:
             self.read_bytes_buffer = b''
-            # print('header_idx := self.read_bytes_buffer.find(params')
             return
-        # if self.read_bytes_buffer[header_idx+1] != params.Host_FT.JtcStatus.value:
-        #     self.read_bytes_buffer = b''
-        #     return
-        # print('header_idx:', header_idx)
+        if self.read_bytes_buffer[header_idx+1] != params.Host_FT.JtcStatus.value:
+            self.read_bytes_buffer = b''
+            return
         nd = int.from_bytes(self.read_bytes_buffer[header_idx + 2:header_idx + 4], 'big', signed=False)
         if len(self.read_bytes_buffer) < (header_idx + nd):
             return
@@ -195,39 +192,39 @@ class RSComm:
 
         return mb_frame
 
-    def read_jtc_status(self, frame):
-        output = [jtc_vars.JTCStatus(), jtc_vars.CANStatus(), trajectory.Trajectory(), [jtc_vars.JointStatus()] * 6]
-        output[0].jtcFsm = frame[9]
-        output[0].jtcErrors = int.from_bytes(frame[10:12], 'big', signed=False)
-        output[0].jtcOccurredErrors = int.from_bytes(frame[12:14], 'big', signed=False)
-        output[0].jtcInitStatus = frame[14]
-        output[0].jointsInitStatus = frame[15]
-        # TRAJ
-        output[2].currentStatus = frame[16]
-        output[2].numCurrentPoint = int.from_bytes(frame[17:21], 'big', signed=False)
-        output[0].jtcFricType = frame[21]
+    # def read_jtc_status(self, frame):
+    #     output = [jtc_vars.JTCStatus(), jtc_vars.CANStatus(), trajectory.Trajectory(), [jtc_vars.JointStatus()] * 6]
+    #     output[0].jtcFsm = frame[9]
+    #     output[0].jtcErrors = int.from_bytes(frame[10:12], 'big', signed=False)
+    #     output[0].jtcOccurredErrors = int.from_bytes(frame[12:14], 'big', signed=False)
+    #     output[0].jtcInitStatus = frame[14]
+    #     output[0].jointsInitStatus = frame[15]
+    #     # TRAJ
+    #     output[2].currentStatus = frame[16]
+    #     output[2].numCurrentPoint = int.from_bytes(frame[17:21], 'big', signed=False)
+    #     output[0].jtcFricType = frame[21]
 
-        output[1].canStatus = frame[22]
-        output[1].canStatusFlags = int.from_bytes(frame[23:27], 'big', signed=False)
-        output[1].canOccurredFlags = int.from_bytes(frame[27:31], 'big', signed=False)
+    #     output[1].canStatus = frame[22]
+    #     output[1].canStatusFlags = int.from_bytes(frame[23:27], 'big', signed=False)
+    #     output[1].canOccurredFlags = int.from_bytes(frame[27:31], 'big', signed=False)
 
-        for i in range(6):
-            offset = 31 + i * 22
-            output[3][i].currentFsm = frame[offset]
-            output[3][i].mcCurrentError = frame[offset + 1]
-            output[3][i].mcOccurredError = frame[offset + 2]
-            output[3][i].currentError = frame[offset + 3]
-            output[3][i].currentWarning = frame[offset + 4]
-            output[3][i].internalErrors = int.from_bytes(frame[offset + 5:offset + 7], 'big', signed=False)
-            output[3][i].internalOccurredErrors = int.from_bytes(frame[offset + 7:offset + 9], 'big', signed=False)
+    #     for i in range(6):
+    #         offset = 31 + i * 22
+    #         output[3][i].currentFsm = frame[offset]
+    #         output[3][i].mcCurrentError = frame[offset + 1]
+    #         output[3][i].mcOccurredError = frame[offset + 2]
+    #         output[3][i].currentError = frame[offset + 3]
+    #         output[3][i].currentWarning = frame[offset + 4]
+    #         output[3][i].internalErrors = int.from_bytes(frame[offset + 5:offset + 7], 'big', signed=False)
+    #         output[3][i].internalOccurredErrors = int.from_bytes(frame[offset + 7:offset + 9], 'big', signed=False)
 
-            [output[3][i].pos] = struct.unpack('>f', frame[offset + 9:offset + 13])
-            [output[3][i].vel] = struct.unpack('>f', frame[offset + 13:offset + 17])
-            [output[3][i].torque] = struct.unpack('>f', frame[offset + 17:offset + 21])
+    #         [output[3][i].pos] = struct.unpack('>f', frame[offset + 9:offset + 13])
+    #         [output[3][i].vel] = struct.unpack('>f', frame[offset + 13:offset + 17])
+    #         [output[3][i].torque] = struct.unpack('>f', frame[offset + 17:offset + 21])
 
-            output[3][i].temperature = frame[offset + 22]
+    #         output[3][i].temperature = frame[offset + 22]
 
-        return output
+    #     return output
 
     # killme
 
